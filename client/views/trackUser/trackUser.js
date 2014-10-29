8ce9758a7b4497368d65abe8f5319517859609b1
richardavendano <avendano.richard@gmail.com>
2014-10-28 17:34:16 -0700

    fixed spacing

--- client/views/trackUser/trackUser.js
+++ client/views/trackUser/trackUser.js
@@ -1,36 +1,36 @@
 angular.module('myApp.trackUser', [] )
 
 .controller('trackUserController', function($scope) {
-	$scope.trackUser = function(user) {
-		$scope.count = 0;
-		$scope.loading = true;
-		$scope.show = false;
-		$scope.userSubmitted;
-		$scope.userKarma;
-		$scope.userToTrack;
-		$scope.update;
-		$scope.updateMessage;
+  $scope.trackUser = function(user) {
+    $scope.count = 0;
+    $scope.loading = true;
+    $scope.show = false;
+    $scope.userSubmitted;
+    $scope.userKarma;
+    $scope.userToTrack;
+    $scope.update;
+    $scope.updateMessage;
 
-		var url = "https://hacker-news.firebaseio.com/v0/user/";
-		var fullUrl = url.concat(user);
-		var hackerNewsRef = new Firebase(fullUrl);
-		hackerNewsRef.on('value', function (snapshot) {
-			$scope.$apply(function() {
-				$scope.count += 1;
-				$scope.loading = false;
-				$scope.show = true;
-				$scope.userSubmitted = snapshot.val().submitted;
-				$scope.userKarma = snapshot.val().karma;
-				if ($scope.count > 1) {
-					$scope.update = true;
-					$scope.updateMessage = "There has been an update to this user";
-					setTimeout(function() { $scope.update = false; }, 20000);
-				}
-			});
-		}, function (errorObject) {
-		  console.log('The read failed: ' + errorObject.code);
-		});	
-	}
+    var url = "https://hacker-news.firebaseio.com/v0/user/";
+    var fullUrl = url.concat(user);
+    var hackerNewsRef = new Firebase(fullUrl);
+    hackerNewsRef.on('value', function (snapshot) {
+      $scope.$apply(function() {
+        $scope.count += 1;
+        $scope.loading = false;
+        $scope.show = true;
+        $scope.userSubmitted = snapshot.val().submitted;
+        $scope.userKarma = snapshot.val().karma;
+        if ($scope.count > 1) {
+          $scope.update = true;
+          $scope.updateMessage = "There has been an update to this user";
+          setTimeout(function() { $scope.update = false; }, 20000);
+        }
+      });
+    }, function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
+    }); 
+  }
 });
 
 
