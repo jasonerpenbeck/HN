838aed1c68e09f231b62de8ce3b5176c50a834f1
jamondouglas <terrildouglas@gmail.com>
2014-10-31 13:25:10 -0700

    Modifications to support findPostForWeeks

--- server/database/services/findPostsForWeek.js
+++ server/database/services/findPostsForWeek.js
@@ -1,4 +1,18 @@
+var queryContainer = require('../queries/queryContainer.js').topicQueries;
+var csv = require('csv-to-json');
+
 exports.findPostsForWeek = function(req) {
+  var x = __dirname;
+  var y = __dirname.lastIndexOf('/');
+  console.log(x.slice(y));
+  console.log(x);
+
+  var query = req.query.param;
+  // console.log(queryContainer);
+  // console.log(queryContainer[query]);
+  console.log(__dirname+""+queryContainer[query]);
+  var json = csv.parse(__dirname+""+queryContainer[query]);
+  console.dir(json);
   return {
     result:'Request received',
     data: 'ok'
