f166872e1c171091c469a63d63988779ac0e94fe
richardavendano <avendano.richard@gmail.com>
2014-10-31 12:02:53 -0700

    fixed files

--- client/views/chart/chart.js
+++ client/views/chart/chart.js
@@ -1,12 +1,4 @@
 angular.module('chart',['storedData'])
 .controller('ChartController', function($scope, CurrentData) {
-  $scope.data = {
-    topics: [
-      {name: 'meteor'},
-      {name: 'angular'},
-      {name: 'lodash'},
-      {name: 'underscore'}
-    ]
-  };
   $scope.graphData = CurrentData.graphData;
-})
+});
