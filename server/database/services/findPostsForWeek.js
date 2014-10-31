b1487c6c841cd7040917454fe1da1553fb21b67d
jamondouglas <terrildouglas@gmail.com>
2014-10-31 14:27:38 -0700

    Fix for req object parsing

--- server/database/services/findPostsForWeek.js
+++ server/database/services/findPostsForWeek.js
@@ -3,21 +3,9 @@ var csv = require('csv-to-json');
 
 exports.findPostsForWeek = function(req) {
   var dirPath = __dirname.split('/services');
-  console.log(dirPath);
-  // console.log(y);
-  // console.log(x.slice(y));
-  // console.log(x);
 
   var query = req.query.topic;
   
-  // console.log(queryContainer);
-  // console.log(queryContainer[query]);
-  console.log(dirPath+""+queryContainer[query]);
-
-  var json = csv.parse(dirPath[0]+''+queryContainer[query]);
-  console.dir(json);
-  return {
-    result:'Request received',
-    data: 'ok'
-  };
+  var json = csv.parse(dirPath[0]+''+queryContainer[req.query.param]);
+  return json;
 };
