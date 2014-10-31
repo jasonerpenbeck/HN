71b61cf7da222b971d4036b04396cf618bc6c863
jamondouglas <terrildouglas@gmail.com>
2014-10-31 11:52:36 -0700

    Add object to pass back csv file location.

---
+++ server/database/queries/queryContainer.js
@@ -0,0 +1,10 @@
+exports.topicQueries = {
+  angular: './angular.csv',
+  backbone: './backbone.csv',
+  bootstrap: './bootstrap.csv',
+  d3: './d3.csv',
+  decryption: './decryption.csv',
+  drupal: './drupal.csv',
+  ember: './ember.csv',
+  encryption: './encryption.csv',
+};
