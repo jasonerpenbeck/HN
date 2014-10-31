5fbabbddf76089f8576cf7bbd184113f6d7aef38
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-30 22:46:30 -0700

    Finished pre-canned queries

--- server/SampleQuery/top20_stories_1m_meteor.sql
+++ server/SampleQuery/top20_stories_1m_meteor.sql
@@ -1,8 +1,18 @@
 -- top 20 stories on a topic in the last month
 -- topic = string match in title
+
+-- last day
+-- 86400
+-- last week
+-- 604800
+-- last month
+-- 2592000
+-- last year
+-- 31104000
+
 SELECT * 
 	FROM stories 
-	WHERE title LIKE '%meteor%' 
-	AND time BETWEEN unix_timestamp(NOW()) - 2592000 AND unix_timestamp(NOW()) 
+	WHERE title LIKE '%jquery%' 
+	AND time BETWEEN (1413150888 - 86400) AND 1413150888 
 	ORDER BY score DESC, time DESC 
 	LIMIT 20;
